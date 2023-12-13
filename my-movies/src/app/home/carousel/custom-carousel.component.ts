642da534edf80b771a70ad198370ceaa3fd048db
lizavr <elizavetarabetskaya@gmail.com>
2023-12-14 02:03:58 +0400

    fix: carousel

--- my-movies/src/app/home/carousel/custom-carousel.component.ts
+++ my-movies/src/app/home/carousel/custom-carousel.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CatalogService } from '../../catalog/catalog.service';
 import { Subscription } from 'rxjs';
 
@@ -9,65 +9,25 @@ import { Subscription } from 'rxjs';
 })
 export class CustomCarouselComponent implements OnInit, OnDestroy {
   subscription: Subscription | undefined;
+  isDataLoaded: boolean = false;
 
-  slides: any[] = new Array(5).fill({
-    id: -1,
-    src: '',
-    title: '',
-    subtitle: '',
-  });
+  slides: any[] = [];
 
-  constructor(private catalogService: CatalogService, private cdRef: ChangeDetectorRef) {}
+  constructor(private catalogService: CatalogService) {}
 
   ngOnInit() {
-    // this.slides[0] = {
-    //   id: 0,
-    //   src: '../../../assets/img/velikolepnyy-vek-scaled.jpg',
-    //   title: 'Magnificent Century',
-    //   subtitle:
-    //     'Popular Turkish historical drama series depicting the life of Sultan Suleiman the Magnificent and his court in the 16th century.',
-    // };
-    // this.slides[1] = {
-    //   id: 1,
-    //   src: '../../../assets/img/cherno-belaya-lyubov-scaled.jpg',
-    //   title: 'Black and White Love',
-    //   subtitle:
-    //     'Turkish romantic drama series about the passionate but challenging relationship between a rebellious young woman and a wealthy businessman.',
-    // };
-    // this.slides[2] = {
-    //   id: 2,
-    //   src: '../../../assets/img/chernaya-lyubov.jpg',
-    //   title: 'Black Love',
-    //   subtitle:
-    //     'Nihan, the daughter of a wealthy man on the brink of bankruptcy, and Kemal, the son of an ordinary barber, meet on a bus and fall in love at first sight, despite their differences and belonging to completely different worlds',
-    // };
-    // this.slides[3] = {
-    //   id: 3,
-    //   src: '../../../assets/img/chernaya-lyubov.jpg',
-    //   title: 'Black Love',
-    //   subtitle:
-    //     'Nihan, the daughter of a wealthy man on the brink of bankruptcy, and Kemal, the son of an ordinary barber, meet on a bus and fall in love at first sight, despite their differences and belonging to completely different worlds',
-    // };
-    // this.slides[4] = {
-    //   id: 4,
-    //   src: '../../../assets/img/chernaya-lyubov.jpg',
-    //   title: 'Black Love',
-    //   subtitle:
-    //     'Nihan, the daughter of a wealthy man on the brink of bankruptcy, and Kemal, the son of an ordinary barber, meet on a bus and fall in love at first sight, despite their differences and belonging to completely different worlds',
-    // };
     this.subscription = this.catalogService
-      .getThreeMovies()
+      .getPopularMovies()
       .subscribe((cards) => {
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 10; i++) {
           this.slides[i] = {
-              id: i,
-              src: '../../../assets/img/chernaya-lyubov.jpg',
-              title: 'Black Love',
-              subtitle:
-                'Nihan, the daughter of a wealthy man on the brink of bankruptcy, and Kemal, the son of an ordinary barber, meet on a bus and fall in love at first sight, despite their differences and belonging to completely different worlds',
-            }
+            id: cards[i].id,
+            src: `https://image.tmdb.org/t/p/w1280${cards[i].backdrop_path}`,
+            title: cards[i].title,
+            subtitle: cards[i].overview,
+          };
         }
-        console.log(this.slides);
+        this.isDataLoaded = true;
       });
   }
 
